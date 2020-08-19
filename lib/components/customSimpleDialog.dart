6e17bc801d8426b0d7a49c1c5a63bb50b5c12ed2
tgeng1 <geng.tian@jsure.com>
2020-08-19 18:16:45 +0800

    modify

---
+++ lib/components/customSimpleDialog.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomSimpleDialog extends StatelessWidget{
+
+  const CustomSimpleDialog({Key key, }): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+
+    );
+  }
+}
